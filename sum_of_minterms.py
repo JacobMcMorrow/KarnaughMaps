aca04176a1e0217e3c524fb5efcff98c9de75a2c
JacobMcMorrow <jacobmcmorrow@rogers.com>
2019-01-14 23:47:40 -0500

    Added __str__ function to SumOfMinterms.

--- sum_of_minterms.py
+++ sum_of_minterms.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 
+from minterm import Minterm
+
 class SumOfMinterms:
-    def __init__(self, num_inputs):
-        self.num_inputs = num_inputs
+    def __init__(self, minterms):
+        self.minterms = minterms
+
+    def __str__(self):
+        res = ' + '.join(str(minterm) for minterm in minterms)
+        return res
+
+    __repr__ = __str__
