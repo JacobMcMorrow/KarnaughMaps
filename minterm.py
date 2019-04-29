a0b305bc1f99f1849d13f0ddc45402eddac83769
JacobMcMorrow <jacobmcmorrow@rogers.com>
2019-04-29 17:16:58 -0400

    Corrected number of minterm variables in representation.

--- minterm.py
+++ minterm.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 
 class Minterm:
-    def __init__(self, row):
+    def __init__(self, row, variables):
         self.row = row
+        self.variables = variables
 
     def _convert_row_binary(self):
         '''Return a binary representation of the row number'''
-        return "{0:b}".format(self.row)
+        binary = "{0:b}".format(self.row)
+        if len(binary) < self.variables:
+            for i in range(self.variables - len(binary)):
+                binary = "0" + binary
+        return binary
 
     def get_table_format(self):
         res = ""
